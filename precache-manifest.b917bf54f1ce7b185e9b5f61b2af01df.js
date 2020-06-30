3c62d3b9d74b0ffb46eb0932a0e9c7a65529923b
Nicolas <thenico0509@gmail.com>
2020-06-29 23:54:58 -0400

    Updates

---
+++ precache-manifest.b917bf54f1ce7b185e9b5f61b2af01df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d5a239d328ffb3a5c612450f8e226a",
+    "url": "/Wh1t3-H4wk/Construccion.git/index.html"
+  },
+  {
+    "revision": "e0655be8ccfc0ef8932f",
+    "url": "/Wh1t3-H4wk/Construccion.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f28b71d2d3b7c6e9e1db",
+    "url": "/Wh1t3-H4wk/Construccion.git/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e0655be8ccfc0ef8932f",
+    "url": "/Wh1t3-H4wk/Construccion.git/static/js/2.df3cd2ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Wh1t3-H4wk/Construccion.git/static/js/2.df3cd2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f28b71d2d3b7c6e9e1db",
+    "url": "/Wh1t3-H4wk/Construccion.git/static/js/main.4c0eec64.chunk.js"
+  },
+  {
+    "revision": "51498e1f0e40546c2f90",
+    "url": "/Wh1t3-H4wk/Construccion.git/static/js/runtime-main.8a0b0e4e.js"
+  }
+]);
