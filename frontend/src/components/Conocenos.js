3ea090700a58a5abd30cc35b4f269bfb77023159
Sebastian Cancino <sebcan1998@gmail.com>
2020-06-30 11:43:40 -0400

    Minor fixes

--- frontend/src/components/Conocenos.js
+++ frontend/src/components/Conocenos.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 class Conocenos extends React.Component {
   componentDidMount() {
@@ -6,18 +6,41 @@ class Conocenos extends React.Component {
   }
 
   render() {
-    return(
+    return (
       <section class="page-section about-heading">
-        <div class="container"><img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="assets/img/about.jpg" alt="cafeteria"/>
+        <div class="container">
+          <img
+            class="img-fluid rounded about-heading-img mb-3 mb-lg-0"
+            src={process.env.PUBLIC_URL + "/assets/img/about.jpg"}
+            alt="cafeteria"
+          />
           <div class="about-heading-content">
             <div class="row">
               <div class="col-lg-10 col-xl-9 mx-auto">
                 <div class="bg-faded rounded p-5">
-                  <h2 class="section-heading mb-4"><span class="section-heading-upper">Café Fuerte, Raices Fuertes</span><span class="section-heading-lower">Acerca de Nuestro Café</span></h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper ipsum quis nulla euismod, vel tempus est condimentum. Cras urna purus, scelerisque in consequat in, commodo sit amet sem. Etiam ultricies pellentesque
-                      dolor sed vestibulum. Integer vitae ligula eget urna posuere euismod. Nulla facilisi.&nbsp;</p>
-                  <p>Pellentesque suscipit dolor sit amet iaculis finibus. Duis tincidunt lacinia tortor, sed efficitur nisi mattis ut. Fusce elit elit, posuere at tincidunt vitae, rhoncus non ante. Cras iaculis ligula commodo lectus tristique
-                      pulvinar. Etiam sed purus ac ex lobortis faucibus sit amet ac enim.</p>
+                  <h2 class="section-heading mb-4">
+                    <span class="section-heading-upper">
+                      Café Fuerte, Raices Fuertes
+                    </span>
+                    <span class="section-heading-lower">
+                      Acerca de Nuestro Café
+                    </span>
+                  </h2>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam ullamcorper ipsum quis nulla euismod, vel tempus est
+                    condimentum. Cras urna purus, scelerisque in consequat in,
+                    commodo sit amet sem. Etiam ultricies pellentesque dolor sed
+                    vestibulum. Integer vitae ligula eget urna posuere euismod.
+                    Nulla facilisi.&nbsp;
+                  </p>
+                  <p>
+                    Pellentesque suscipit dolor sit amet iaculis finibus. Duis
+                    tincidunt lacinia tortor, sed efficitur nisi mattis ut.
+                    Fusce elit elit, posuere at tincidunt vitae, rhoncus non
+                    ante. Cras iaculis ligula commodo lectus tristique pulvinar.
+                    Etiam sed purus ac ex lobortis faucibus sit amet ac enim.
+                  </p>
                 </div>
               </div>
             </div>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -7,16 +7,44 @@ import { Link } from "react-router-dom";
 function NavBar(props) {
   return (
     <Navbar variant="dark" bg="dark" expand="lg" id="mainNav" sticky="top">
-      <Navbar.Brand className="text-uppercase d-lg-none text-expanded" as={Link} to="/">Donde José Villar</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Brand
+        className="text-uppercase d-lg-none text-expanded"
+        as={Link}
+        to="/"
+      >
+        Donde José Villar
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse>
-        <Nav.Link className="text-white text-uppercase" as={Link} to="/">Inicio</Nav.Link>
-        <Nav.Link className="text-white text-uppercase" as={Link} to="/conocenos">Conócenos</Nav.Link>
-        <Nav.Link className="text-white text-uppercase" as={Link} to="/registrarse">Regístrarse</Nav.Link>
-        <Nav.Link className="text-white text-uppercase" as={Link} to="/codigos">Códigos</Nav.Link>
+        <Nav.Link className="text-white text-uppercase" as={Link} to="/">
+          Inicio
+        </Nav.Link>
+        <Nav.Link
+          className="text-white text-uppercase"
+          as={Link}
+          to="/conocenos"
+        >
+          Conócenos
+        </Nav.Link>
+        <Nav.Link
+          className="text-white text-uppercase"
+          as={Link}
+          to="/registrarse"
+        >
+          Regístrate
+        </Nav.Link>
+        <Nav.Link className="text-white text-uppercase" as={Link} to="/codigos">
+          Códigos
+        </Nav.Link>
       </Navbar.Collapse>
       <Navbar.Collapse className="justify-content-end">
-        <Nav.Link className="text-white text-uppercase" as={Link} to="/cuenta/nacho123@gmail.com">Cuenta</Nav.Link>
+        <Nav.Link
+          className="text-white text-uppercase"
+          as={Link}
+          to="/cuenta/nacho123@gmail.com"
+        >
+          Cuenta
+        </Nav.Link>
       </Navbar.Collapse>
       <Carro carro={props.carro} eliminarDeCarro={props.eliminarDeCarro} />
     </Navbar>
