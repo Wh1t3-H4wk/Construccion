288c5b5c63a7527424ed14b3395cf97556ac6f05
Nicolas <thenico0509@gmail.com>
2020-06-29 23:36:58 -0400

    intento de publicación

---
+++ frontend/build/precache-manifest.efb6e586969d5577cd316fa11a92285f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3aea3800ce9a7b653df57a1a5ca16a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0655be8ccfc0ef8932f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d4515a4a2b4dba1478c7",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e0655be8ccfc0ef8932f",
+    "url": "/static/js/2.df3cd2ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.df3cd2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4515a4a2b4dba1478c7",
+    "url": "/static/js/main.c7e8ad7a.chunk.js"
+  },
+  {
+    "revision": "fc89c6dfca9d2cfd55ac",
+    "url": "/static/js/runtime-main.f315e7e4.js"
+  }
+]);
